9710fb01d38675446604b56db602f61ed74ddc95
s1n7ax <srineshnisala@gmail.com>
2019-03-10 13:32:15 +0530

    externalized material component modules
     - material imports are separated to material module to cleanup app module
     - import the material module exports to app module
     - add code format of some files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+
+
+const MaterialComponents = [
+];
+
+@NgModule({
+    declarations: [],
+    imports: [MaterialComponents],
+    exports: [MaterialComponents]
+})
+export class MaterialModule {
+}
