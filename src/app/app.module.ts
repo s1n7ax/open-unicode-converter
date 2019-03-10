3454e3fc9b4949f20780a277cc35fc3f2e532c93
s1n7ax <srineshnisala@gmail.com>
2019-03-10 15:00:09 +0530

    add ng component
     - add header component for the application header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
